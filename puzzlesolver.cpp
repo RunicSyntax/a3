306f745fceb3f3f2a04b25eab9c4f3be27570d36
RunicSyntax <grimurarnar@gmail.com>
2024-09-29 13:45:47 +0000

    Might be working

--- puzzlesolver.cpp
+++ puzzlesolver.cpp
@@ -18,9 +18,9 @@ class Socket {
     int socket_type;
 
   int send_n_recv(int port, char message[], size_t message_size, char *recv_buffer[], size_t buffer_size) {
-    
+    return 0;
   }
-}
+};
 
 struct pseudo_header {
     u_int32_t source_address;  // Source IP address
@@ -94,6 +94,112 @@ void solve_port(const char* ip, int port, int challenge_num, char message[], siz
         // Print out response from port 
         std::cout << "\nResponse from port: " << buffer << "\n\n";
 
+
+        if (challenge_num == 2) {
+            const unsigned char *last_six = (unsigned char *)(buffer + len - 6);
+
+            // Extract expected checksum from the response
+            uint16_t expected_checksum = (last_six[0] << 8) | last_six[1];
+
+            // Extract source IP from the response
+            uint32_t source_ip;
+            std::memcpy(&source_ip, last_six + 2, 4);
+
+            char package[sizeof(struct iphdr) + sizeof(struct udphdr)];
+            memset(package, 0, sizeof(package));
+
+            // Create IP header
+            struct iphdr* iph = (struct iphdr*) package;
+            iph->ihl = 5;
+            iph->version = 4;
+            iph->tos = 0;
+            iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr));
+            iph->id = htons(5500);
+            iph->frag_off = 0;
+            iph->ttl = 64;
+            iph->protocol = IPPROTO_UDP;
+            iph->check = 0;
+            iph->saddr = source_ip;  // Source IP is already in network byte order
+            iph->daddr = inet_addr(ip);  // Destination IP in network byte order
+
+            // Calculate IP header checksum
+            iph->check = calculate_checksum((unsigned short *)iph, sizeof(struct iphdr) / 2);
+
+            // Create UDP header
+            struct udphdr *udph = (struct udphdr *) (package + sizeof(struct iphdr));
+            udph->dest = htons(port);
+            udph->len = htons(sizeof(struct udphdr));
+            udph->check = 0;
+
+            // Set up the pseudo-header
+            struct pseudo_header psh;
+            psh.source_address = source_ip;
+            psh.destination_address = inet_addr(ip);
+            psh.placeholder = 0;
+            psh.protocol = IPPROTO_UDP;
+            psh.udp_length = htons(sizeof(struct udphdr));
+
+            // Prepare buffer for checksum calculation
+            int pseudo_packet_size = sizeof(struct pseudo_header) + sizeof(struct udphdr);
+            char pseudo_packet[pseudo_packet_size];
+            memcpy(pseudo_packet, &psh, sizeof(struct pseudo_header));
+            memcpy(pseudo_packet + sizeof(struct pseudo_header), udph, sizeof(struct udphdr));
+
+            // Adjust the source port to match the expected checksum
+            uint16_t source_port;
+            bool checksum_matched = false;
+            for (source_port = 10000; source_port < 65535; source_port++) {
+                udph->source = htons(source_port);
+
+                // Recalculate UDP checksum
+                memcpy(pseudo_packet + sizeof(struct pseudo_header), udph, sizeof(struct udphdr));
+                uint16_t calculated_checksum = calculate_checksum((unsigned short *)pseudo_packet, pseudo_packet_size / 2);
+
+                if (calculated_checksum == expected_checksum) {
+                    udph->check = htons(calculated_checksum);
+                    checksum_matched = true;
+                    break;
+                }
+            }
+
+            if (!checksum_matched) {
+                std::cerr << "Failed to find a source port that matches the expected checksum." << std::endl;
+                close(sock);
+                return;
+            }
+
+            // Send the packet
+            struct sockaddr_in dest_addr = addr;  // Copy destination address
+            dest_addr.sin_port = udph->dest;      // Ensure destination port is set
+
+            int package_size = sizeof(struct iphdr) + sizeof(struct udphdr);
+            int send_result = sendto(sock, package, package_size, 0, (struct sockaddr*)&dest_addr, sizeof(dest_addr));
+            if (send_result < 0) {
+                perror("Error sending custom reply");
+            }
+
+            // Wait for a response
+            FD_ZERO(&fds);
+            FD_SET(sock, &fds);
+            timeout.tv_sec = 1;
+            timeout.tv_usec = 0;
+            second_response = select(sock + 1, &fds, nullptr, nullptr, &timeout);
+
+            if (second_response > 0) {
+                int length = recvfrom(sock, second_buffer, sizeof(second_buffer) - 1, 0, nullptr, nullptr);
+                if (length >= 0) {
+                    second_buffer[length] = '\0';  // Null-terminate the received message
+                    std::cout << "Response to custom reply: " << second_buffer << std::endl;
+                } else {
+                    perror("Failed to receive response to custom reply");
+                }
+            } else {
+                std::cout << "No response received to custom reply." << std::endl;
+            }
+        }
+
+
+        /*
         if (challenge_num == 2){
 
           const unsigned char *last_six = (unsigned char *)(buffer + len - 6);
@@ -109,16 +215,18 @@ void solve_port(const char* ip, int port, int challenge_num, char message[], siz
           memset(package, 0, sizeof(package));
           
           // Designate data length
-          uint16_t data_len = 2;
+          //uint16_t data_len = 2;
 
           // Make the IP header
           struct iphdr* iph = (struct iphdr*) package;
+
+          int source_port = 12345;
 
           // Fill in IP header information
           iph->ihl = 5;
           iph->version = 4;
           iph->tos = 0;
-          iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr) + data_len);
+          iph->tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr));
           iph->id = htons(5500);
           iph->frag_off = 0;
           iph->ttl = 64;
@@ -128,20 +236,20 @@ void solve_port(const char* ip, int port, int challenge_num, char message[], siz
           iph->daddr = htonl(inet_addr(ip));
 
 
-          iph->check = calculate_checksum((unsigned short *)package, sizeof(struct iphdr) / 2);
+          iph->check = calculate_checksum((unsigned short *)&iph, sizeof(struct iphdr) / 2);
 
           // Make the UDP header
           struct udphdr *udph = (struct udphdr *) (package + sizeof(struct iphdr));
 
           // Fill in UDP header information
-          udph->source = htons(5500);
+          udph->source = htons(source_port);
           udph->dest = htons(port);
-          udph->len = htons(sizeof(struct udphdr) + data_len);
+          udph->len = htons(sizeof(struct udphdr));
           udph->check = 0;
 
           // Define data pointer for checksum modification
-          uint16_t *data = (uint16_t *)(package + sizeof(struct iphdr) + sizeof(struct udphdr));
-          *data = 0;
+          //uint16_t *data = (uint16_t *)(package + sizeof(struct iphdr) + sizeof(struct udphdr));
+          //k*data = 0;
 
           // Pseudo-header for checksum calculation
           struct pseudo_header psh;
@@ -149,28 +257,23 @@ void solve_port(const char* ip, int port, int challenge_num, char message[], siz
           psh.destination_address = htonl(inet_addr(ip));
           psh.placeholder = 0;
           psh.protocol = IPPROTO_UDP;
-          psh.udp_length = htons(sizeof(struct udphdr) + data_len);
+          psh.udp_length = htons(sizeof(struct udphdr));
 
           // Buffer for pseudo-header + UDP packet
           u_char pseudo_packet[30];
           memcpy(pseudo_packet, &psh, sizeof(struct pseudo_header));
-          memcpy(pseudo_packet + sizeof(struct pseudo_header), udph, sizeof(struct udphdr) + data_len);
+          memcpy(pseudo_packet + sizeof(struct pseudo_header), udph, sizeof(struct udphdr));
+          
+          unsigned short initial_udp_checksum = calculate_checksum(&psh, &udph, nullptr, 0);
 
-          uint16_t checksum = calculate_checksum((unsigned short*)pseudo_packet, sizeof(pseudo_packet));
-          for (uint16_t i = 0; i < 65535; i++) {
-            udph->check = 0;
-            *data = i;
-            memcpy(pseudo_packet + sizeof(struct pseudo_header) + sizeof(struct udphdr), data, sizeof(data_len));
-            checksum = calculate_checksum((unsigned short*)pseudo_packet, sizeof(pseudo_packet));
-            if(checksum = expected_checksum){
-              break;
-            }
-          }
+          short checksum_difference = expected_checksum - initial_udp_checksum;
 
-          std::cout << "Calculated checksum: " << std::hex << checksum << std::endl;
-          udph->check = htons(checksum);
+          source_port += checksum_difference / 2;
+          udph->source = htons(source_port);
+          
+          udph->check = htons(expected_checksum);
 
-          int package_size = sizeof(struct iphdr) + sizeof(struct udphdr) + data_len;
+          int package_size = sizeof(struct iphdr) + sizeof(struct udphdr);
 
           int send = sendto(sock, package, package_size, 0, (struct sockaddr*)&addr, sizeof(addr));
 
@@ -187,7 +290,7 @@ void solve_port(const char* ip, int port, int challenge_num, char message[], siz
           } else {
               perror("Failed to receive response to custom reply");
           }
-        }
+        }*/
       }
     } else {
       // Port sends no response within timeout
@@ -200,7 +303,6 @@ void solve_port(const char* ip, int port, int challenge_num, char message[], siz
   //Close socket
   close(sock);
 }
-
 
 int main (int argc, char *argv[]) {
   if (argc != 6) {
